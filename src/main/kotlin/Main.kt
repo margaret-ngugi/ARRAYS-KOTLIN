295a663db03b1e4dde9129e4812c02162609ffe0
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-04-05 13:50:29 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var friends = arrayOf("Ann", "Ben", "Chris", "Diana")
+   var numbers = arrayOf(32,1,657,543,2,789,32,43,89,0)
+    println(friends.sortedArray().contentToString())
+    println(friends.contentToString())
+
+
+
+
+}
+fun numbers(){
+    var numbers = arrayOf(1,4,3,5)
+}
